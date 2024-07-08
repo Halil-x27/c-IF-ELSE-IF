71cdf64754a202239f7fc5e9f9da123d8b49a93d
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-08 12:59:31 +0300

    Add files via upload

---
+++ IF-ELSE-IF/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>6 && time<11)
+            {
+                Console.WriteLine("Günaydin");
+            }
+            else if (time<=18)
+            {
+                Console.WriteLine("iyi günler");
+            }
+            else{
+                Console.WriteLine("iyi geceler");
+            }
+
+            string sonuc = time<=18 ? "iyi günler" : "iyi geceler";
+
+            sonuc = time>=6 && time<11 ? "günaydin" : time<=18 ? "iyi günler" : "iyi geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
